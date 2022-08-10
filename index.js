6b2616d58bb30d9e4c014ab38eb45941601c4af2
Tim <dalytimdaly@gmail.com>
2022-08-10 17:51:05 -0600

    commit tree 223a7c3cdd32da1d9858a0b723f7c33d131cac35

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+
+function writeCards(cards, event) {
+    for (let i=0; i < 3; i++)
+    console.log(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+    return cards
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+
+
+function countDown () {
+   for (let i = 10; i > -1; i--)
+    console.log(i)
+}
+
+countDown(10)
